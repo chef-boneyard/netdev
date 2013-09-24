9494e9853440de890b1def8fb3bbd2200416a857
Seth Chisamore <schisamo@opscode.com>
2013-09-24 16:08:36 -0400

    Add initial boilerplate for Chefspec tests

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'berkshelf',  '~> 2.0'
+gem 'chefspec',   '~> 2.0'
+gem 'foodcritic', '~> 2.2'
+
+group :test do
+  gem 'junos-ez-stdlib', :git => 'https://github.com/Juniper/ruby-junos-ez-stdlib.git', :tag => 'v0.2.0_20130819_1'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,55 @@
+#
+# Author:: Seth Chisamore <schisamo@opscode.com>
+#
+# Copyright:: Copyright (c) 2013 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'berkshelf'
+require 'chefspec'
+
+# Stop 'already initialized constant' warnings. These
+# warnings appear because we load the library files
+# twice: once in this file and once by the Chef run.
+$VERBOSE = nil
+
+# load all library files for easy mocking
+libs = File.expand_path('../../libraries', __FILE__)
+$LOAD_PATH.unshift(libs) unless $LOAD_PATH.include?(libs)
+Dir[File.join(libs, '*.rb')].each do |lib|
+  require File.basename(lib, '.rb')
+end
+
+Berkshelf.ui.mute do
+  berksfile = Berkshelf::Berksfile.from_file('Berksfile')
+  berksfile.install(:path => 'vendor/cookbooks')
+end
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # specify metadata with symobls only (ie no '=> true' required)
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+end
+
+RSpec::Matchers.define :have_constant do |const|
+  match do |owner|
+    owner.const_defined?(const)
+  end
+end
