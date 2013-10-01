868d7a4ef49b54bf145475ee57ab191f2c8ec3d1
Seth Chisamore <schisamo@opscode.com>
2013-10-01 11:20:15 -0400

    Rubocop is a pedant about those double quotes.
    https://github.com/bbatsov/ruby-style-guide#strings

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -7,7 +7,7 @@ include Serverspec::Helper::DetectOS
 RSpec.configure do |c|
   if ENV['ASK_SUDO_PASSWORD']
     require 'highline/import'
-    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
+    c.sudo_password = ask('Enter sudo password: ') { |q| q.echo = false }
   else
     c.sudo_password = ENV['SUDO_PASSWORD']
   end
