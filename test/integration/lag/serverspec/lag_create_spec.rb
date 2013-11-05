99f59358f73863c9791fe85032615cdb91842bf5
Seth Chisamore <schisamo@opscode.com>
2013-11-05 11:04:43 -0500

    Add integration test coverage for `link aggregation groups`.
    This is commented out for now. Still need to hunt down some issues
    related to automating LAG configuration.

---
+++ test/integration/lag/serverspec/lag_create_spec.rb
@@ -0,0 +1,15 @@
+require 'serverspec'
+require 'pathname'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+%w{ ge-0/0/1 ge-0/0/2 }.each do |l2_interface|
+  describe command("cli show config interfaces #{l2_interface}") do
+    it { should return_stdout(/802.3ad ae0;/) }
+  end
+end
+
+describe command('cli show configuration interfaces ae0') do
+  it { should return_stdout(/minimum-links 1;/) }
+end
